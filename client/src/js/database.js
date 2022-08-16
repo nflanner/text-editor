91725d9d694e0df1bd5b20ec28677a98ca0ac105
Nolan Flannery <nolanflannery@gmail.com>
2022-08-16 12:47:36 -0700

    done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { header } from './header';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -15,18 +16,19 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Post to the database');
+  console.log(content);
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jatrDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ content: content });
+  // Use the .put() method on the store and pass in the content.
+  const request = store.put({ id: 1, content: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -51,8 +53,13 @@ export const getDb = async () => {
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  if (result.length) {
+    console.log(result.map(object => object.content));
+    console.log('result', result);
+    return result[0].content;
+  } else {
+    return header;
+  }
 };
 
 initdb();
